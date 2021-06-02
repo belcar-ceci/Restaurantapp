f2228ff4d3c11a02d21b97a144ee20bbaa1d81f9
nicko785 <you@example.com>
2021-06-02 12:55:12 +0200

    carrito

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function ocultar()
+{
+    document.getElementById('index').style.display = 'none';
+    document.getElementById('carrito').style.display = 'block';
+}
+function mostrar()
+{
+    document.getElementById('index').style.display = 'block';
+    document.getElementById('carrito').style.display = 'none';
+
+}
