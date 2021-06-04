a901920b7fdded810c3d55d3c97a86d4abb6c276
nicko785 <you@example.com>
2021-06-04 13:47:40 +0200

    arraysInicio

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+import { menuComida } from "menu.js";
+
 let mainView = document.getElementById('index');
 let cartView = document.getElementById('carrito');
 
@@ -11,4 +13,19 @@ function mostrar()
     mainView.style.display = 'block';
     cartView.style.display = 'none';
 
+}
+
+let elementInput = document.querySelector(".cantidad");
+elementInput.value = 0;
+
+function sumarItem() 
+{
+    elementInput.value++;
+}
+function restarItem()
+{
+    if(elementInput.value > 0 )
+    {
+        elementInput.value--;
+    }
 }
---
+++ js/menu.js
@@ -0,0 +1,34 @@
+let menuComida
+[
+    {
+        id: 01,
+        precio: 52.00,
+        titulo: 'Chicken Veggi Salad',
+        descripcion: 'With cheese saude',
+        imagen: 'img/section comida/ensaladapollo.jpg'
+    }
+    ,
+    {
+        id: 02,
+        precio: 23.00,
+        titulo: 'Mutton Curry',
+        descripcion: 'with basmati rice',
+        imagen: 'img/section comida/curry.jpg'
+    }
+    ,
+    {
+        id: 03,
+        precio: 21.00,
+        titulo: 'French Fry',
+        descripcion: 'with beef bacon',
+        imagen: 'img/section comida/baconfrenchfries.jpg'
+    }
+    ,
+    {
+        id: 04,
+        precio: 52.00,
+        titulo: 'Chicken Veggi Salad',
+        descripcion: 'With cheese saude',
+        imagen: 'img/section comida/ensaladapollo.jpg'
+    }
+]
